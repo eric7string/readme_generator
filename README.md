fde8e454d57a7fee6a917142e8391c3895b86444
eric7string <153536554+eric7string@users.noreply.github.com>
2024-08-26 18:31:09 -0400

    Initial commit, initial JS structure

---
+++ README.md
@@ -0,0 +1,24 @@
+# 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+##Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Test](#test)
+- [License](#license)
+- [Questions](#questions)
+## Description
+
+## Installation
+
+## Usage
+
+## Contribution
+
+## Test
+
+## License
+        This project is licensed under the MIT license.
+## Questions
+If you have any questions, you can reach me through my GitHub [github/](https://www.github.com/) or via email at .
---
+++ index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+import generateMarkdown from "./utils/generateMarkdown.js";
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the usage information for your project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Please list and contributors to your project.",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions for your project?",
+        name: "test",
+    },
+    {
+        type: "list",
+        message: "What license would you like to use?",
+        name: "license",
+        choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log("Creating README.md");
+            writeToFile("README.md", generateMarkdown(response));
+            
+        });
+}
+
+// Function call to initialize app
+init();
